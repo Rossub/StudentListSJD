f0b7ab07acdc1cf6e94a8310a751e6e14f008fe5
Rossub <rosh2060an@gmail.com>
2024-05-23 11:40:48 -0400

    Fourth Commit.

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -2,9 +2,15 @@ package studentlist;
 
 public class Student {
     String name;
+    String ID;
 
-    public Student(String name){
+    public Student(String name, String ID){
         this.name = name;
+        this.ID = ID;
+    }
+    
+    public String getID(){
+        return ID;
     }
 
     public String getName(){
